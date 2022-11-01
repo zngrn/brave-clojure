e93f74bfa37b35bca094df427dc626b828c6644d
zngrn <vaibhavjhingran@gmail.com>
2022-11-01 10:12:08 +0100

    Tumbling through the clojure core functions

--- src/brave_clojure/core_functions.clj
+++ src/brave_clojure/core_functions.clj
@@ -56,3 +56,100 @@
   "A key takeaway from reduce is it helps
    derive a new value from a seq data structure.")
 
+;; take, drop, take-while, drop-while
+(take 3 '(1 2 3 4 5 6 7)) ;; => (1 2 3)
+(drop 2 [1 2 3 4 5]) ;; => (3 4 5)
+
+(def monthly-analysis
+  [{:month 1 :games 8 :goals 1 :assists 2 :yellow 2 :red 0}
+   {:month 2 :games 9 :goals 3 :assists 4 :yellow 2 :red 0}
+   {:month 3 :games 6 :goals 2 :assists 5 :yellow 1 :red 0}
+   {:month 4 :games 8 :goals 3 :assists 3 :yellow 1 :red 0}
+   {:month 5 :games 8 :goals 2 :assists 4 :yellow 3 :red 0}
+   {:month 6 :games 5 :goals 0 :assists 4 :yellow 1 :red 0}])
+
+(take-while #(< (:month %) 4) monthly-analysis) ;; returns the first quarter stats
+(drop-while #(< (:month %) 4) monthly-analysis) ;; returns the second quarter stats
+
+(def aggregate-stats #(reduce + %))
+
+(defn total-games-played
+  [numbers]
+  (aggregate-stats (map #(% :games) numbers)))
+
+(defn total-goals-scored
+  [numbers]
+  (aggregate-stats (map #(% :assists) numbers)))
+
+(defn total-assists-given
+  [numbers]
+  (aggregate-stats (map #(% :goals) numbers)))
+
+(total-games-played monthly-analysis) ;; => 44
+(total-goals-scored monthly-analysis) ;; => 22
+(total-assists-given monthly-analysis) ;; => 11
+
+;; further refining this to DRY friendly
+(defn total-stats
+  [numbers key]
+  (aggregate-stats (map #(% key) numbers)))
+
+(total-stats monthly-analysis :goals) ;; => 11
+
+;; filter
+(filter #(>= (:yellow %) 3) monthly-analysis) ;; => ({:month 5, :games 8, :goals 2, :assists 4, :yellow 3, :red 0})
+(comment
+  "filter can end up going over all the data. 
+  Use wisely for large data-sets.")
+
+;; some
+(comment
+  "Test condition for the first truthy value.")
+(some #(> (:red %) 1) monthly-analysis) ;; => nil
+(some #(> (:yellow %) 1) monthly-analysis) ;; => true
+
+;; sort & sort-by
+(sort '(-123 123 8 0 12)) ;; => (-123 0 8 12 123)
+(sort-by count ["a" "a" "aa" "bb" "ccc"]) ;; => ("a" "a" "aa" "bb" "ccc")
+
+;; concat
+(concat [1 2 3] [-1 -2 0]) ;; => (1 2 3 -1 -2 0)
+
+;; lazy seqs
+(comment 
+  "lazy seqs are seqs whose members aren't computed
+   until they are accessed. Computing them is called
+   'realising'. Deferring this execution is for efficiency.")
+
+(def first-team-players
+  {0 {:fit? true :number 1 :available? true :name "foo"}
+   1 {:fit? false :number 4 :available? false :name "bar"}
+   2 {:fit? true :number 7 :available? true :name "baz"}
+   3 {:fit? true :number 10 :available? false :name "foz"}})
+
+(defn player-details
+  [number]
+  (Thread/sleep 1000)
+  (get first-team-players number))
+
+(defn fit?
+  [record]
+  (and (:fit? record)
+       (:available? record)
+  record))
+
+(defn identify-player
+  [numbers]
+  (first (filter fit?
+                 (map player-details numbers))))
+
+(time (player-details 0))
+;; => "Elapsed time: 1001.178908 msecs"
+;; => {:fit? true, :number 1, :available? true :name "foo"}
+
+;; infinite sequences
+(concat ["nah" "nah" "nah"] (take 2 (repeat "na na na nah!")) ["Hey Jude!"])
+;; => ("nah" "nah" "nah" "na na na nah!" "na na na nah!" "Hey Jude!")
+
+(take 5 (repeatedly (fn [] (rand-int 10)))) 
+;; will print a random number 5 times within the range of 0..10

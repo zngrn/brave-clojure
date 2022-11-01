a3535493407328199d3a4901b8ce1b1f5b5cd931
zngrn <vaibhavjhingran@gmail.com>
2022-11-01 10:16:20 +0100

    Update project details

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject brave-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Collection of notes from Clojure for the Brave and True."
+  :url "https://github.com/zngrn/brave-clojure"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot brave-clojure.core
   :target-path "target/%s"

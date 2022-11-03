a6c3f3058d809263b451fdad87cfc9c785a800bd
zngrn <vaibhavjhingran@gmail.com>
2022-11-03 10:19:03 +0100

    Advancing along the chapter of functional programming

--- src/brave_clojure/functional_programming.clj
+++ src/brave_clojure/functional_programming.clj
@@ -41,4 +41,47 @@
    Since core data structures in clojure are immutable, side
    effects are mitigated.")
 
+(comment
+  "Functional alternative to mutation is recursion.")
 
+;; function composition over mutation
+(require '[clojure.string :as s])
+(defn clean-text
+  [text]
+  (s/replace (s/trim text) #"lol" "*laugh out loud!*"))
+(clean-text "This is hilarious! lol      ") ;; => "This is hilarious! *laugh out loud!*"
+
+(comment
+  "combining functions like trim and replace where the output
+   of one function is used as the arguement of another function
+   is called function composition."
+  )
+
+(comment
+  "In OOP, the function of classes is to protect the change
+   of state of mutation and prevent unwanted modification
+   of objects, specifically private data. This isn't needed
+   with immutable data structures.
+   Additionally, class functions are tightly bound and limit
+   the use whereas functions here can be applied to any argument
+   of a similar data type. This decouples the function and 
+   programs to a small set of abstractions.")
+
+;; comp
+;; creating a new function from the composition of
+;; any number of functions.
+((comp inc *) 2 5) ;; => 11
+;; A more succinct example of comp
+(def dota-character-stats
+  {:name "Ember Spirit"
+   :attributes {
+                :intelligence 7
+                :agility 9
+                :strength 8}})
+(def attribute-intelligence (comp :intelligence :attributes))
+(def attribute-agility (comp :agility :attributes))
+(def attribute-strength (comp :strength :attributes))
+
+(attribute-intelligence dota-character-stats) ;; => 7
+(attribute-agility dota-character-stats) ;; => 9
+(attribute-strength dota-character-stats) ;; => 8
